13abb3da424ee4bf2e4bd8e839557fe8a4dc4884
Martin Breining <martin.breining@gmail.com>
2014-11-26 21:41:31 -0800

    Extract duplicate code into webpack.common.config

--- webpack/webpack.hot.config.js
+++ webpack/webpack.hot.config.js
@@ -1,42 +1,10 @@
+var config = require("./webpack.common.config");
 var webpack = require("webpack");
-var path = require("path");
 
-module.exports = {
-  devtool: "eval-source-map",
-
-  context: __dirname, //  + "/../app/assets/javascripts",
-  entry: [
-    "webpack-dev-server/client?http://localhost:3000",
-    "webpack/hot/dev-server",
-    "./scripts/webpack_only",
-    "./assets/javascripts/example"
-  ],
-  // Note, this file is not actually saved, but used by the express server
-  output: {
-    filename: "express-bundle.js",
-    path: __dirname
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  resolve: {
-    root: [ path.join(__dirname, "scripts"), path.join(__dirname, "assets/javascripts"),
-            path.join(__dirname, "assets/stylesheets") ],
-    extensions: ["", ".webpack.js", ".web.js", ".js", ".jsx", ".scss", ".css", "config.js"]
-  },
-  module: {
-    loaders: [
-      { ***REMOVED*** require.resolve("react"), loader: "expose?React" },
-      { ***REMOVED*** /\.jsx$/, loaders: ["react-hot", "es6", "jsx?harmony"] },
-      { ***REMOVED*** /\.css$/, loader: "style-loader!css-loader" },
-      { ***REMOVED*** /\.scss$/, loader: "style!css!sass?outputStyle=expanded&imagePath=/assets/images"},
-
-      // the url-loader uses DataUrls.
-      // the file-loader emits files.
-      { ***REMOVED*** /\.woff$/,   loader: "url-loader?limit=10000&minetype=application/font-woff" },
-      { ***REMOVED*** /\.ttf$/,    loader: "file-loader" },
-      { ***REMOVED*** /\.eot$/,    loader: "file-loader" },
-      { ***REMOVED*** /\.svg$/,    loader: "file-loader" }
-    ]
-  }
-};
+config.entry.push("webpack-dev-server/client?http://localhost:3000",
+                  "webpack/hot/dev-server",
+                  "./scripts/webpack_only");
+config.output = { filename: "express-bundle.js", // this file is never saved
+                  path: __dirname };
+config.plugins = [ new webpack.HotModuleReplacementPlugin() ];
+module.exports = config;
