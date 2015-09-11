63bc7e8d54fe7aeab7e740642126db0a7d1f48b5
dylangrafmyre <dgrafmyre@gmail.com>
2015-09-11 12:19:31 -1000

    Remove require expose jQuery and jQuery-ujs from script/rails_only Move add jquery and jquery-ujs to entry point in web.common.config.js Add expose-loader for jQuery module/loaders with web.common.config.js

--- client/webpack.common.config.js
+++ client/webpack.common.config.js
@@ -6,7 +6,7 @@ module.exports = {
 
   // the project dir
   context: __dirname,
-  entry: ['./assets/javascripts/App'],
+  entry: ['jquery', 'jquery-ujs', './assets/javascripts/App'],
 
   resolve: {
     root: [
@@ -20,6 +20,7 @@ module.exports = {
 
       // React is necessary for the client rendering:
       {***REMOVED*** require.resolve('react'), loader: 'expose?React'},
+      {***REMOVED*** require.resolve('jquery'), loader: 'expose?jQuery'}
     ],
   },
 };
