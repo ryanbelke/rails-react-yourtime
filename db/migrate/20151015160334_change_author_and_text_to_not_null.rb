babe56d3cb70b5d1ef46e372ae24963c147a93f8
ryan <ryanj.belke@gmail.com>
2017-11-02 22:53:32 -0500

    removed migration for comments

--- db/migrate/20151015160334_change_author_and_text_to_not_null.rb
+++
@@ -1,6 +0,0 @@
-class ChangeAuthorAndTextToNotNull < ActiveRecord::Migration
-  def change
-    change_column_null(:purchasor, :author, false, "")
-    change_column_null(:purchasor, :text, false, "")
-  end
-end
