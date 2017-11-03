1e460adfe2b1bed0c69ba62c66d3e7c4c908a147
ryan <ryanj.belke@gmail.com>
2017-11-03 10:09:38 -0500

    added custom css into the layout and WORKING

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,3 +2,5 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
+#Added to precompile the custom css
+Rails.application.config.assets.precompile += %w( application_static.css )
