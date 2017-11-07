50f8eabbbb2a63280bf0fb9ca961d635887b3802
ryan <ryanj.belke@gmail.com>
2017-11-06 20:06:36 -0600

    removed integer gallons migration

--- db/migrate/20171030002859_add_info_to_purchases.rb
+++
@@ -1,7 +0,0 @@
-class AddInfoToPurchases < ActiveRecord::Migration[5.1]
-  def change
-    add_column :purchases, :gallons, :integer
-    add_column :purchases, :fuel_type, :string
-    add_column :purchases, :price, :integer
-  end
-end
