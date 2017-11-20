ac1f15c730bf7f4997718eb36fe9217fcfa39f5a
ryan <ryanj.belke@gmail.com>
2017-11-20 13:47:52 -0600

    added reference for locations

--- db/migrate/20171119053307_add_info_to_locations.rb
+++ db/migrate/20171119053307_add_info_to_locations.rb
@@ -3,5 +3,6 @@ class AddInfoToLocations < ActiveRecord::Migration[5.1]
     add_column :locations, :location_name, :string
     add_column :locations, :location_address, :string
     add_column :locations, :location_description, :string
+    add_reference :locations, :workplace, foreign_key: true
   end
 end
