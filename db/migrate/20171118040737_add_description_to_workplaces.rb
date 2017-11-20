4657611373fe8b75ae3c32ad7ff5916ce14b8ecf
ryan <ryanj.belke@gmail.com>
2017-11-19 23:02:19 -0600

    added workplace migration info

--- db/migrate/20171118040737_add_description_to_workplaces.rb
+++ db/migrate/20171118040737_add_description_to_workplaces.rb
@@ -1,5 +1,7 @@
 class AddDescriptionToWorkplaces < ActiveRecord::Migration[5.1]
   def change
+    add_column :workplaces, :workplace_name, :string
+    add_column :workplaces, :workplace_address, :string
     add_column :workplaces, :workplace_description, :string
   end
 end
