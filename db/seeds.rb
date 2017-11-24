bbda3d92b49ceb0e56fe18bff88fec903e1272b2
ryan <ryanj.belke@gmail.com>
2017-11-24 15:01:26 -0600

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,7 +44,36 @@ Workplace.create!(workplace_name: 'Silicon Labs')
 Workplace.create!(workplace_name: 'Spredfast')
 Workplace.create!(workplace_name: 'Not Listed')
 
+@workplace_silicon = Workplace.find_by id: 1
+@workplace_spredfast = Workplace.find_by id: 2
 
+
+@workplace_silicon.locations.create!(location_name: 'Building 400', location_address: '450 West Cesar Chavez',
+                                    location_description: 'Description for building 400')
+@workplace_silicon.locations.create!(location_name: 'Building 600', location_address: '450 West Cesar Chavez',
+                                     location_description: 'Description for building 600')
+@workplace_spredfast.locations.create!(location_name: 'Building 1000', location_address: '350 West Cesar Chavez',
+                                     location_description: 'Description for building 1000')
+@location1 = Location.find_by id: 1
+@location2 = Location.find_by id: 2
+
+@location1.services.create!(service_name: 'Oil Change', service_description: 'Change the oil')
+@location1.services.create!(service_name: 'Radiator Flush', service_description: 'Flush the radiator')
+
+@location2.services.create!(service_name: 'Tire Rotation', service_description: 'Roate The Tires')
+@location2.services.create!(service_name: 'Oil Change', service_description: 'Chang the oil')
+
+
+@date = Date.parse('2017-11-14')
+@user = User.find_by id: 1
+@user.appointments.create!(appointment_status: 'Pending', appointment_date: @date,
+                          service_id: 1, appointment_price: '$29.99', appointment_location: 'Building 400')
+@user.appointments.create!(appointment_status: 'Scheduled', appointment_date: @date,
+                           service_id: 1, appointment_price: '$29.99', appointment_location: 'Building 400')
+@user.appointments.create!(appointment_status: 'Error', appointment_date: @date,
+                           service_id: 1, appointment_price: '$29.99', appointment_location: 'Building 400')
+@user.appointments.create!(appointment_status: 'Completed', appointment_date: @date,
+                           service_id: 1, appointment_price: '$29.99', appointment_location: 'Building 400')
 
 
 # Service
