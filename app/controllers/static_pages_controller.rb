577bd495d7b6fa79aeabdd5950f41779ed17d6d3
ryan <ryanj.belke@gmail.com>
2017-11-29 22:19:36 -0600

    adding in method to prevent error upon logout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,8 +5,8 @@ class StaticPagesController < ApplicationController
     @workplaces = Workplace.all.sort_by &:created_at
     @categories = Category.order(:category_name)
     @services = Service.all
-
-    if current_user.admin?
+#check to see if a user exsists, if so is the user admin to see all the feeds
+    if current_user && current_user.admin?
       @feed_items = Workplace.where.not(workplace_name: "Not Listed").paginate(page: params[:page])
       @location_feed_items = Location.all.paginate(page: params[:page])
       @service_feed_items = Service.all.paginate(page: params[:page])
