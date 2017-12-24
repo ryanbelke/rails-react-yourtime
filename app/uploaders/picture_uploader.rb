cd96a722856eaf6fb7a5a54a205d7f3c92c84111
ryan <ryanj.belke@gmail.com>
2017-12-23 22:36:09 -0600

    added region s3

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,18 +1,12 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
-    #storage :fog
+    storage :fog
   else
     storage :file
   end
-  # storage :fog
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -20,7 +14,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   end
   # Add a white list of extensions which are allowed to be uploaded.
   def extension_white_list
-    %w(jpg jpeg gif png svg)
+    %w(jpg jpeg gif png)
   end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
