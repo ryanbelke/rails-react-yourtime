df8ead563fc69705d7b0eb03971c71caa8e1371d
ryan <ryanj.belke@gmail.com>
2017-12-24 11:44:06 -0600

    re-did uploader and carrierwave config after xcode install

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,18 +2,16 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
-   process resize_to_limit: [400, 400]
+  # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+
   if Rails.env.production?
     storage :fog
-    #storage :file
-
   else
     storage :file
   end
-  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -21,6 +19,7 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  # Add a white list of extensions which are allowed to be uploaded.
   def extension_white_list
     %w(jpg jpeg gif png)
   end
