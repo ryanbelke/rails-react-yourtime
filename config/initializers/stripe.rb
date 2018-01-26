01f259a8150b32b6fec0bb4fe58afc5260433c3a
ryan <ryanj.belke@gmail.com>
2018-01-25 20:03:00 -0600

    added in stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,15 +1,8 @@
-if Rails.env.development?
 Rails.configuration.stripe = {
 
-      :publishable_key => ENV['PUBLISHABLE_KEY_TEST'],
-      :secret_key      => ENV['SECRET_KEY_TEST']
+    :publishable_key => ENV['PUBLISHABLE_KEY'],
+    :secret_key      => ENV['SECRET_KEY']
 }
-else
-  Rails.configuration.stripe = {
-      :publishable_key => ENV['PUBLISHABLE_KEY_LIVE'],
-      :secret_key => ENV['SECRET_KEY_LIVE']
-  }
-end
 
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
