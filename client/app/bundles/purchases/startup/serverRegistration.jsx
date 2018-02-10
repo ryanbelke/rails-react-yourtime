aaacb89d12f773529aeb62557ed4784ce6b685a4
ryan <ryanj.belke@gmail.com>
2018-02-10 12:30:46 -0600

    switching to add react application for checkout

--- client/app/bundles/purchases/startup/serverRegistration.jsx
+++ client/app/bundles/purchases/startup/serverRegistration.jsx
@@ -2,13 +2,13 @@
 import ReactOnRails from 'react-on-rails';
 
 import App from './App';
-
+import RouterApp from './ServerRouterApp';
 import routerCommentsStore from '../store/routerCommentsStore';
 import commentsStore from '../store/commentsStore';
 
 ReactOnRails.register(
   {
-    App,
+    App, RouterApp,
   },
 );
 
